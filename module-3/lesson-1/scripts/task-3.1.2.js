6fcf9f6035fef6d45258f675e2223a4e0ea0521e
Haliema26 <77581177+Haliema26@users.noreply.github.com>
2021-02-11 20:11:36 +0000

    Update task-3.1.2.js
    Add alert and console.log. document.write

---
+++ module-3/lesson-1/scripts/task-3.1.2.js
@@ -0,0 +1,10 @@
+/*
+this is a 
+multiple line 
+comment
+*/
+
+//this is a single line comment
+alert('Hello from the alert box');// this pops up with analert message
+console.log('Hello from the console');// showa a message in the consle
+document.write('hello from the document.write');//prints a message to the secreen
