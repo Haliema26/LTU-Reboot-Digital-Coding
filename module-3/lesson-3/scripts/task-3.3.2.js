a32b4bd9dd49a88ca902ca09739ca964046bbcec
Haliema26 <77581177+Haliema26@users.noreply.github.com>
2021-02-18 19:58:37 +0000

    JavaS Task

---
+++ module-3/lesson-3/scripts/task-3.3.2.js
@@ -0,0 +1,29 @@
+function drinkOrder( size, drink ){
+
+
+let drink=drinkName.toLowerCase();
+
+switch(drinkLowerCase) {
+    case 'cola':
+        console.log ('You have order a'+ size + drink);
+        
+        break;
+        case 'lemonade':
+        console.log('you have order a' + size +drink);
+        break;
+        case'oranged':
+        console.log( 'you have order a' + size+ drink);
+        break;
+
+
+
+        default:
+            console.log('error');
+            break;
+
+
+
+}
+
+}
+drinkOrder('smal','cola');
