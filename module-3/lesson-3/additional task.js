1febafac18b1270046627bbf7b6abb9f0a7942cf
Haliema26 <77581177+Haliema26@users.noreply.github.com>
2021-02-18 20:49:34 +0000

    tasks Js

---
+++ module-3/lesson-3/additional task.js
@@ -0,0 +1,21 @@
+function  seasonWear(season) {
+    switch ( season) {
+        case 'spring':
+            console.log(`it is ${season}`);
+            break;
+            case 'summer':
+                console.log(`it is ${season}`):
+                break;
+                case 'autumn':
+                    console.log(`it is ${season}`);
+                    break;
+                    case 'winter' :
+                        console.log(`it is ${season}`);
+                        break;
+                        default :
+                        console.log( `it is ${season}`);
+
+
+    }
+}
+seasonWear ('winter');
---
+++ module-3/lesson-3/scripts/task-3.3.3.js
@@ -0,0 +1,25 @@
+function calculator( number1, number2, operator) {
+
+switch(operator){
+case'+':
+console.log(`${number1}+${number2}=${number1+number2}`);
+break;
+case '-':
+console.log(`${number1}+${number2}=${number1+number2}`);
+break;
+case '*':
+    console.log(`${number1}+${number2}=${number1+number2}`);
+    break;
+    case '/':
+        console.log(`${number1}+${number2}=${number1+number2}`);
+        break;
+default:
+    console.log(`${operator} is not valid operator`);
+    break;
+}
+
+  }
+
+
+    
+}
