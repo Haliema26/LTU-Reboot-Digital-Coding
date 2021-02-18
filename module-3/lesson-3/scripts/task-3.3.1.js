c15d0891c3fcedcb67bda44a5bde58f9df70da65
Haliema26 <77581177+Haliema26@users.noreply.github.com>
2021-02-18 19:26:58 +0000

    Update task-3.3.1.js

---
+++ module-3/lesson-3/scripts/task-3.3.1.js
@@ -0,0 +1,13 @@
+function percentageCalculator( number, percentage) {
+    var percentageOf= ( number*percentage) / 100;
+
+    return percentageOf;
+
+}
+
+
+
+
+consol.log( percentageCalculator(135,30));
+
+
